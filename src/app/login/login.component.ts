42af5a38a10cefc311a9445bd895e2ec8554b3b0
Allen Yu (Wicresoft North America) <v-jiyu7@microsoft.com>
2019-05-15 17:43:40 +0800

    add some login html & css

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    templateUrl: `./login.component.html`
+    templateUrl: `./login.component.html`,
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
