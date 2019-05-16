dc316df037d2576d7c2fc9838e421fc3a9ccfd09
Allen Yu (Wicresoft North America) <v-jiyu7@microsoft.com>
2019-05-16 17:32:39 +0800

    login interface design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent }
 ];
 
