b37ad919f80a11e2e98911c503cb600449d2a0c9
Allen Yu (Wicresoft North America) <v-jiyu7@microsoft.com>
2019-05-21 15:12:42 +0800

    login code

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,39 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 
 @Component({
     templateUrl: `./login.component.html`,
     styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+    loginForm: FormGroup = new FormGroup({
+        userName: new FormControl('', this.isNullOrEmpty()),
+        password: new FormControl('', this.isNullOrEmpty())
+    });
 
+    public get userName() {
+        return this.loginForm.get('userName');
+    }
+
+    public get password() {
+        return this.loginForm.get('password');
+    }
+
+    public login() {
+        console.log(this.loginForm.value);
+    }
+
+    public focus(control: FormControl) {
+        control.markAsUntouched();
+    }
+
+    private isNullOrEmpty(): ValidatorFn {
+        return (control: AbstractControl): { [key: string]: any } | null => {
+            const value: string = control.value;
+            if (!value || value.trim().length === 0) {
+                return { nullOrEmpty: true };
+            }
+            return null;
+        };
+    }
 }
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
+    imports: [
+        ReactiveFormsModule,
+        CommonModule],
     declarations: [LoginComponent]
 })
 export class LoginModule {
